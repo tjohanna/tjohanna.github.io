2edc342f8f582270ba39b91e7bd699cd67339eb7
tjohanna <31165913+tjohanna@users.noreply.github.com>
2021-09-07 18:16:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ $('.quoteBox').click(function() {
 
 
 // display a random image
+var myPix = new Array("https://i.ibb.co/tc8t7yt/k1.png","https://i.ibb.co/tL3XBFc/k4.png","https://i.ibb.co/2dhzJgq/k2.png","https://i.ibb.co/3RdhV6M/k3.png","https://i.ibb.co/5v1xc82/k5.png");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("randomimg").src = myPix[randomNum];
+}
+choosePic();
+
+
+
+// display a random image
 // var image = new Array ();
 // image[0] = "https://i.ibb.co/tc8t7yt/k1.png";
 // image[1] = "https://i.ibb.co/tL3XBFc/k4.png";
@@ -32,13 +43,3 @@ $('.quoteBox').click(function() {
 // var x = Math.floor(size*Math.random())
 
 // $('#randomimg').attr('src',image[x]);
-
-
-// display a random image
-var myPix = new Array("https://i.ibb.co/tc8t7yt/k1.png","https://i.ibb.co/tL3XBFc/k4.png","https://i.ibb.co/2dhzJgq/k2.png","https://i.ibb.co/3RdhV6M/k3.png","https://i.ibb.co/5v1xc82/k5.png");
-
-function choosePic() {
-     var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("randomimg").src = myPix[randomNum];
-}
-choosePic();
